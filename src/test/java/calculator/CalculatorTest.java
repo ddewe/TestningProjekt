49edaaad4bcf175c6987e23bfabfdd9b17bc0772
ddewe <ddewee92@gmail.com>
2020-12-04 13:35:54 +0100

    First commit, added class Calculator.

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    void calculatorClassExists() {
+        Calculator calculator = new Calculator();
+    }
+
+}
