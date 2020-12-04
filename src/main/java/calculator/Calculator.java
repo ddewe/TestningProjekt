a7d225eaf3979b3578943d5ea592c0d093578ea2
ddewe <ddewee92@gmail.com>
2020-12-04 13:47:37 +0100

    Added addMethodReturnsZeroWhenInputIsEmpty

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,9 @@
 package calculator;
 
 public class Calculator {
+
+    public int add(String numbers) {
+        return 0;
+    }
+
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -2,11 +2,25 @@ package calculator;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
 
     @Test
     void calculatorClassExists() {
         Calculator calculator = new Calculator();
+    }
+
+    @Test
+    void addMethodExists() {
+        Calculator calculator = new Calculator();
+        calculator.add("");
+    }
+
+    @Test
+    void addMethodReturnsZeroWhenInputIsEmpty() {
+        Calculator calculator = new Calculator();
+        assertEquals(0, calculator.add(""));
     }
 
 }
